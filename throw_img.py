491a0f73972ef5bbf93d0a19b7ad11de7c5c8fdf
Tomoaki Ando <tomoaki_ando@TomoakinoMacBook-Pro.local>
2019-11-09 17:35:26 +0900

    add factor of interaction btw request and FastAPI

---
+++ throw_img.py
@@ -0,0 +1,16 @@
+import requests, sys, json, base64
+from pathlib import Path
+
+path = Path(sys.argv[-1])
+url  = "http://127.0.0.1:8000/pred/"
+
+with open(path, 'rb') as f:
+    # f.read() is bytes type
+    # 1. For security, we need to encode by base64 (still bytes type)
+    # 2. FastAPI requires json type: bytes -> str (json cannot dump bytes type)
+    enc_img = base64.b64encode(f.read()).decode('utf-8')
+    body = json.dumps({'enc_img': enc_img})
+    r = requests.post(url, data=body)
+
+print({'pred' :r.text})
+
