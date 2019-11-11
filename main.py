33ff1100cf1e2682202ea7bcba469b01058d2e2a
Tomoaki Ando <tomoaki_ando@TomoakinoMacBook-Pro.local>
2019-11-11 20:00:48 +0900

    add cnn-layers and gitignore file

--- main.py
+++ main.py
@@ -1,31 +1,57 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
 from pathlib import Path
 from PIL import Image
-import os, base64, io
-
+import os
+import base64
+import io
 import numpy as np
 import torch
 from torch import nn
 
-
 app = FastAPI()
+
+
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x):
+        return self.func(x)
+
+
+def flatten(x):
+    return x.view(len(x), -1)
+
+
+def mnist_resize(x):
+    return x.view(-1, 1, 28, 28)
+
 
 def predict(img_path):
     # decode for loading img
     img = base64.b64decode(img_path.enc_img)
     # convert RGB into gray scale
     img = Image.open(io.BytesIO(img)).resize((28, 28)).convert("L")
-    img = torch.tensor(np.array(img)).view(-1).float()
+    img = torch.tensor(np.array(img)).view(-1, 1, 28, 28).float()
 
     # load pre-trained model
-    weight = Path(os.getcwd())/"weight.pth"
-    model  = nn.Sequential(nn.Linear(img.shape[-1], 50), nn.ReLU(), nn.Linear(50, 10))
+    weight = Path(os.getcwd()) / "weight_cnn.pth"
+    model = nn.Sequential(
+        Lambda(mnist_resize),
+        nn.Conv2d(1, 8, 5, 2, 2), nn.ReLU(),
+        nn.Conv2d(8, 16, 3, 2, 1), nn.ReLU(),
+        nn.Conv2d(16, 32, 3, 2, 1), nn.ReLU(),
+        nn.Conv2d(32, 32, 3, 2, 1), nn.ReLU(),
+        nn.AdaptiveAvgPool2d(1),
+        Lambda(flatten),
+        nn.Linear(32, 10)
+    )
     model.load_state_dict(torch.load(weight))
 
     # inference
-    pred = model(img.view(1, -1))
+    pred = model(img)
     pred = torch.argmax(pred, 1)
 
     return pred.item()
@@ -34,7 +60,7 @@ def predict(img_path):
 class Item(BaseModel):
     enc_img: bytes
 
+
 @app.post("/pred/")
 async def return_pred(file: Item):
     return predict(file)
-
--- throw_img.py
+++ throw_img.py
@@ -1,16 +1,18 @@
-import requests, sys, json, base64
+import requests
+import sys
+import json
+import base64
 from pathlib import Path
 
 path = Path(sys.argv[-1])
-url  = "http://127.0.0.1:8000/pred/"
+url = "http://127.0.0.1:8000/pred/"
 
 with open(path, 'rb') as f:
     # f.read() is bytes type
     # 1. For security, we need to encode by base64 (still bytes type)
-    # 2. FastAPI requires json type: bytes -> str (json cannot dump bytes type)
+    # 2. FastAPI requires json: bytes -> str (json cannot dump bytes type)
     enc_img = base64.b64encode(f.read()).decode('utf-8')
     body = json.dumps({'enc_img': enc_img})
     r = requests.post(url, data=body)
 
-print({'pred' :r.text})
-
+print({'pred': r.text})
