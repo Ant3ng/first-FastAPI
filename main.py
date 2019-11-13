1e218c4365c0b72badf0901f05344312c2112919
Tomoaki Ando <tomoaki_ando@TomoakinoMacBook-Pro.local>
2019-11-13 17:33:18 +0900

    mod directory structure and del unnecessary files

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from pathlib import Path
-from PIL import Image
+from PIL import Image, ImageOps
 import os
 import base64
 import io
 import numpy as np
 import torch
-from fastai_resnet import resnet_model
+from models.spatial_tfm_nw import Stnet
+
 
 app = FastAPI()
+
+
+def normalize(x):
+    mean, std = .1307, .3081
+    return (x - mean) / std
 
 
 def predict(img_path):
@@ -17,15 +23,23 @@ def predict(img_path):
     img = base64.b64decode(img_path.enc_img)
     # convert RGB into gray scale
     img = Image.open(io.BytesIO(img)).resize((28, 28)).convert("L")
+    # pixel of input image is opposite to MNIST
+    img = ImageOps.invert(img)
+
+    # premise for spatial tfm nw is 4d tensor
     img = torch.tensor(np.array(img)).view(-1, 1, 28, 28).float()
 
+    # Scale in training was btw 0 ~ 1, and normalized
+    # So we need to rescale img, and then normalize
+    img = normalize(img / 255)
+
     # load pre-trained model
-    weight = Path(os.getcwd()) / "fastai_resnet.pt"
-    model = resnet_model()
+    weight = Path(os.getcwd()) / "pre-trained/stn_30epoch.pt"
+    model = Stnet()
     model.load_state_dict(torch.load(weight))
 
     # bs=1 prediction with BatchNorm should be in eval mode
-    # Otherwise, you'll encounter error
+    # Otherwise, you'll get an error
     # https://discuss.pytorch.org/t/error-expected-more-than-1-value-per-channel-when-training/26274
     model.eval()
 
